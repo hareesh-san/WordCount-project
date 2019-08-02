98269c666b2d39746f2ec6297f161ed3a47551d4
Hareesh Sandupatla <Harry@Hareeshs-MacBook-Pro.local>
2019-08-02 17:09:43 -0400

    included swagger dependency and annotation

--- src/main/java/com/count/controller/IndexController.java
+++ src/main/java/com/count/controller/IndexController.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class IndexController {
 
     private final Logger logger = LoggerFactory.getLogger(IndexController.class);
