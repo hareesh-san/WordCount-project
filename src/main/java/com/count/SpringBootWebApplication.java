35ab37804c63aacc734c23ae620a548e92f1ad18
Hareesh Sandupatla <Harry@Hareeshs-MacBook-Pro.local>
2019-08-05 15:26:23 -0400

    added Swagger Docket object and  ApiInfo object

--- src/main/java/com/count/SpringBootWebApplication.java
+++ src/main/java/com/count/SpringBootWebApplication.java
@@ -2,7 +2,15 @@ package com.count;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -13,4 +21,27 @@ public class SpringBootWebApplication {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
 
+
+    @Bean
+    public Docket SwaggerConfiguration(){
+        //Return a prepared Docket object.
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/*"))
+                .apis(RequestHandlerSelectors.basePackage("com.count"))
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Word Count Api",
+                "Sample API for finding the word count using SpringBoot and Singleton Design Pattern ",
+                "1.0",
+                "Free",
+                new springfox.documentation.service.Contact("Harish", "http://test.springbootapplication.com", "abc@yahoo.co.in"),
+                "API License",
+                "http://test.springbootapplication.com",
+                Collections.emptyList());
+    }
 }
